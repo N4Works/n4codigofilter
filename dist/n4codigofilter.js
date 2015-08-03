781834ff91b2b896c52616c7447ed2d5820d1d42
Tiago Resende <tiagor87@gmail.com>
2015-08-03 19:28:09 -0300

    Gera arquivo não minificado também

---
+++ dist/n4codigofilter.js
@@ -0,0 +1,29 @@
+"use strict";
+
+;(function(ng)
+{
+  ng
+    .module('n4CodigoFilter', [])
+    .filter('Codigo', function () {
+      return function (codigo, tamanho, caractere) {
+        if (!codigo) {
+          return '';
+        }
+
+        if (isNaN(codigo)) {
+          return codigo;
+        }
+
+        codigo = codigo.toString().trim();
+
+        tamanho = tamanho || 6;
+        caractere = caractere || '0';
+
+        while (codigo.length < tamanho) {
+          codigo = caractere + codigo;
+        }
+
+        return codigo;
+      };
+    });
+}(angular))
